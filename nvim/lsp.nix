6718ad5f21d7ba9393e077612c035606d3ea6a09
knakto <kasichonooo@gmail.com>
2026-03-16 04:15:03 +0700

    add: treesitter lsp cmp

---
+++ nvim/lsp.nix
@@ -0,0 +1,31 @@
+_:
+{
+  ############################
+  # Language Server Protocol #
+  ############################
+  config.vim.lsp.enable = true;
+  config.vim.treesitter.enable = true;
+  config.vim.languages = {
+    enableTreesitter = true;
+    enableExtraDiagnostics = true;
+    enableDAP = true;
+
+    #---- Enable Language ----#
+    clang.enable = true;
+    clang.lsp.enable = true;
+    nix.enable = true;
+    nix.lsp.enable = true;
+    html.enable = true;
+    html.lsp.enable = true;
+    ts.enable = true;
+    ts.lsp.enable = true;
+  };
+
+  ############################
+  #     Completion CMP       #
+  ############################
+  config.vim.autocomplete = {
+    blink-cmp.enable = true;
+    blink-cmp.friendly-snippets.enable = true;
+  };
+}
