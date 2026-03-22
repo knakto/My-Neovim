6a843ce12dc0e123b49e2fe87cbf965a87d2a12a
knakto <kasichonooo@gmail.com>
2026-03-22 16:14:29 +0700

    add feat(tmux): connect tmux to toggleterm

--- nvim/toggleterm.nix
+++ nvim/toggleterm.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
+let
+  tmuxConfigFile = pkgs.writeText "tmux_config.conf" (import ./tmux/tmux.nix {inherit pkgs;}).config;
+in
 {
   vim.terminal.toggleterm = {
     enable = true;
     mappings.open = "<C-\\>";
     setupOpts = {
-      shell = "${pkgs.zsh}/bin/zsh";
+      shell = "${pkgs.tmux}/bin/tmux -f '${tmuxConfigFile}' new-session -A -s nvf";
       direction = "horizontal";
       enable_winbar = true;
     };
